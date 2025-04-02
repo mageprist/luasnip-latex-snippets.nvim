671992ca529776b641d8bfb653583095a9bfeab4
mageprist <gurraholmqvist@gmail.com>
2025-04-02 17:03:26 +0200

    itemize

--- lua/luasnip-latex-snippets/wA.lua
+++ lua/luasnip-latex-snippets/wA.lua
@@ -13,8 +13,8 @@ function M.retrieve(not_math)
     parse_snippet({ trig = "mk", name = "Math" }, "\\( ${1:${TM_SELECTED_TEXT}} \\)$0"),
     parse_snippet({ trig = "dm", name = "Block Math" }, "\\[\n\t${1:${TM_SELECTED_TEXT}}\n.\\] $0"),
     parse_snippet({ trig = "itm", name = "Item" }, "\\item $0"),
-    parse_snippet({ trig = "item", name = "Itemize" }, "\\begin{itemize} \n \\item $0 \n \\end{itemize}"),
-    parse_snippet({ trig = "enum", name = "Enumerate" }, "\\begin{enumerate} \n \\item $0 \n \\end{enumerate}"),
+    parse_snippet({ trig = "item", name = "Itemize" }, "\\begin{itemize}\n\t\\item $0 \n\\end{itemize}"),
+    parse_snippet({ trig = "enum", name = "Enumerate" }, "\\begin{enumerate}\n\t\\item $0 \n\\end{enumerate}"),
   }
 end
 
